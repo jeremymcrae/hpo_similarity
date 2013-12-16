d07517000943b6a1ee255806b4a11eb016241676
Jeremy McRae <jm33@sanger.ac.uk>
2013-12-16 13:20:06 +0000

    - converted loading HPO graph into class  - new file for loading DDG2P genes, HPO terms for participants, and candidate genes

---
+++ load_files.py
@@ -0,0 +1,194 @@
+""" simple functions to load and parse data for HPO terms analyses
+"""
+
+def load_ddg2p(ddg2p_path):
+    """ load a DDG2P gene file, so we can extract HPO terms for each gene
+    
+    Args:
+        ddg2p_path: path to DDG2P file
+    
+    Returns:
+        dictionary of hpo terms indexed by gene name, then inheritance type
+    """
+    
+    f = open(ddg2p_path)
+    
+    # allow for gene files with different column names and positions
+    header = f.readline().strip().split("\t")
+    if "type" in header:
+        gene_label = "gene"
+        confirmed_status_label = "type"
+        inheritance_label = "mode"
+        hpo_label = "hpo_ids"
+    else:
+        raise ValueError("The gene file lacks expected header column names")
+    
+    # get the positions of the columns in the list of header labels
+    gene_column = header.index(gene_label)
+    confirmed_status_column = header.index(confirmed_status_label)
+    inheritance_column = header.index(inheritance_label)
+    hpo_column = header.index(hpo_label)
+    
+    genes = {}
+    for line in f:
+        line = line.strip().split("\t")
+        
+        gene = line[gene_column]
+        inheritance = line[inheritance_column]
+        hpo_terms = line[hpo_column]
+        
+        if gene not in genes:
+            genes[gene] = {}
+        
+        if inheritance not in genes[gene]:
+            genes[gene][inheritance] = set()
+        
+        # split the hpo terms, then update the gene/inheritance set
+        hpo_terms = hpo_terms.split(";")
+        hpo_terms = [x.strip() for x in hpo_terms]
+        genes[gene][inheritance].update(hpo_terms)
+    
+    return genes
+
+class familyHPO(object):
+    """small class to handle HPO terms for family members of a trio
+    """
+    
+    def __init__(self, child_hpo, maternal_hpo, paternal_hpo):
+        """initiate the class
+        
+        Args:
+            child_hpo: HPO string for the proband, eg "HP:0005487|HP:0001363"
+            maternal_hpo: string of HPO terms for the mother
+            paternal_hpo: string of HPO terms for the father
+        """
+        
+        self.child_hpo = self.format_hpo(child_hpo)
+        self.maternal_hpo = self.format_hpo(maternal_hpo)
+        self.paternal_hpo = self.format_hpo(paternal_hpo)
+    
+    def format_hpo(hpo_terms):
+        """ formats a string of hpo terms to a list
+        
+        Args:
+            hpo_terms: string of hpo terms joined with "|" 
+        
+        Returns:
+            list of hpo terms, or None
+        """
+        
+        # account for no hpo terms recorded for a person
+        if hpo_terms.strip() == ".":
+            return None
+        
+        # account for multiple hpo terms for an individual
+        if "|" in hpo_terms:
+            return hpo_terms.strip().split("|")
+        
+        return [hpo_terms.strip()]
+    
+    def get_child_hpo(self):
+        return self.child_hpo
+    
+    def get_maternal_hpo(self):
+        return self.maternal_hpo
+    
+    def get_paternal_hpo(self):
+        return self.paternal_hpo
+
+def load_participants_hpo_terms(pheno_path, alt_id_path):
+    """ loads patient data, and obtains
+    """
+    
+    # loads the decipher to DDD ID mapping file
+    alt_ids = {}
+    f = open(alt_id_path)
+    for line in f:
+        ref_id = line[0]
+        alt_id = line[1]
+        if ":" in alt_id:
+            alt_id = alt_id.split(':"')[0]
+        alt_ids[alt_id] = ref_id
+    
+    # load the phenotype data for each participant
+    f = open(pheno_path)
+    
+    # allow for gene files with different column names and positions
+    header = f.readline().strip().split("\t")
+    if "type" in header:
+        proband_label = "decipher_id"
+        child_hpo_label = "child_hpo"
+        maternal_hpo_label = "maternal_hpo"
+        paternal_hpo_label = "paternal_hpo"
+    else:
+        raise ValueError("The gene file lacks expected header column names")
+    
+    # get the positions of the columns in the list of header labels
+    proband_column = header.index(proband_label)
+    child_hpo_column = header.index(child_hpo_label)
+    maternal_hpo_column = header.index(maternal_hpo_label)
+    paternal_hpo_column = header.index(paternal_hpo_label)
+    
+    participant_hpo = {}
+    for line in f:
+        proband_id = line[proband_column]
+        child_hpo = line[child_hpo_column]
+        maternal_hpo = line[maternal_hpo_column]
+        paternal_hpo = line[paternal_hpo_column]
+        
+        # swap the proband across to the DDD ID if it exists
+        if proband_id in alt_ids:
+            proband_id = alt_ids[proband_id]
+        
+        participant_hpo[proband_id] = familyHPO(child_hpo, maternal_hpo, paternal_hpo)
+    
+    return participant_hpo
+
+def load_candidate_genes(candidate_genes_path):
+    """ loads a list of candidate genes for the participants
+    """
+    
+    f = open(candidate_genes_path)
+    
+     # allow for gene files with different column names and positions
+    header = f.readline().strip().split("\t")
+    if "type" in header:
+        proband_label = "proband"
+        gene_label = "gene_hpo"
+        inheritance_label = "inheritance_hpo"
+    else:
+        raise ValueError("The gene file lacks expected header column names")
+    
+    proband_column = header.index(proband_label)
+    gene_column = header.index(gene_label)
+    inheritance_column = header.index(inheritance_label)
+    
+    genes_index = {}
+    probands_index = {}
+    
+    for line in f:
+        line = line.strip().split("\t")
+        proband_ID = line[proband_column]
+        gene = line[gene_column]
+        inheritance = line[inheritance_column]
+        
+        if gene not in genes_index:
+            genes_index[gene] = set()
+            
+        if proband not in probands_index:
+            probands_index = set()
+        
+        genes_index[gene].update((proband_ID, inheritance))
+        probands_index[proband_ID].update((gene, inheritance))
+    
+    return genes_index, probands_index
+        
+    
+    
+
+
+
+
+
+
+    
--- phenotype_matcher.py
+++ phenotype_matcher.py
@@ -1,159 +1,185 @@
-"""
+""" some graph analyses of HPO terms and their usage in patients and gene hits
 """
 
 # dependencies: 
 #    go-parser (http://bazaar.launchpad.net/~ntamas/+junk/go-parser/files)
 #    networkx (http://networkx.github.io/, pip install --user networkx)
-#    matplotlib (, pip install --user matplotlib)
+#    matplotlib (pip install --user matplotlib)
 #    HPO (http://compbio.charite.de/hudson/job/hpo/ or http://www.human-phenotype-ontology.org/)
 #    DDG2P
 
-import obo
+import os
 import networkx as nx
 import matplotlib as plt
 
+import obo
+import load_files
 
 # load the HPO database as graph database
     # convert terms between database versions, if necessary
 
-# load the DDG2P gene list with HPO terms
-
 # load clinical reporting file, find frequently identified genes, check their HPO terms from DDG2P, 
 # load the patients HPO terms, check for matches between top genes and 
 
+USER_PATH = "/nfs/users/nfs_j/jm33/"
+HPO_PATH = os.path.join(USER_PATH, "apps", "hpo_filter", "hpo_data", "hp.obo")
+CANDIDATE_VARIANTS_PATH = os.path.join(USER_PATH, "clinical_reporting.txt")
 
-def load_hpo_database(hpo_path):
-    """ load the human phenotype ontology (HPO) database in obo format
+ddd_freeze = "/nfs/ddd0/Data/datafreeze/1139trios_20131030/"
+DDG2P_PATH = os.path.join(ddd_freeze, "DDG2P_with_genomic_coordinates_20131107.tsv")
+PHENOTYPES_PATH = os.path.join(ddd_freeze, "phenotypes.shared.20131129.txt")
+ALTERNATE_IDS_PATH = os.path.join(ddd_freeze, "person_sanger_decipher.private.txt")
+
+class loadHPONetwork(object):
+    """ load a HPO obo file into a graph network for subsequent analysis
+    """
     
-    Args:
-        hpo_path: path to HPO obo formatted file
+    def __init__(self, hpo_path):
+        """ load the hpoe file, and process it into a network
+        """
+        header, hpo = self.load_hpo_database(hpo_path)
+        self.create_hpo_graph(header, hpo)
+    
+    def load_hpo_database(self, hpo_path):
+        """ load the human phenotype ontology (HPO) database in obo format
         
-    Returns:
-        parser.header: obo header for file
-        hpo_entries: list of entries in HPO database
-    """
-    
-    parser = obo.Parser(hpo_path)
-    hpo_entries = []
-    for entry in parser:
-        hpo_entries.append(entry)
-    
-    return parser.headers, hpo_entries
-
-def add_hpo_attributes_to_node(graph, node_id, obo_tags):
-    """ add hpo attributes to a graph node
-    
-    Args:
-        graph: networkx graph object
-        node_id: string ID for a graph node
-        obo_tags: tags for an obo entry
-        
-    Returns:
-        nothing, updates the graph node within this function
-    """
-    
-    hpo_keys = ['comment', 'subset', 'xref', 'synonym', 'name', 'created_by', 'creation_date', 'def', 'alt_id']
-    
-    for key in hpo_keys:
-        if key in obo_tags:
-            graph.node[node_id] = obo_tags[key]
-
-def track_alternate_ids(obo_tags, node_id, alt_mapper, ref_mapper):
-    """ keep track of alternate IDs for HPO terms, so we can map between alternate and canonical
-    
-    Args:
-        obo_tags: tags for an obo entry
-        node_id: string ID for a graph node
-        alt_mapper: dict to map from an alternate HPO ID to a canonical HPO IDs
-        ref_mapper: dict to map from a canonical HPO ID to its potential alternate IDs
-    
-    Returns:
-        nothing, updates the dictionaries within this function
-    """
-    
-    # make sure we can convert between the alternate IDs and their HPO ID
-    if "alt_id" in obo_tags:
-        for alt_id in obo_tags["alt_id"]:
-            alt_id  = str(alt_id)
-            alt_mapper[alt_id] = node_id
+        Args:
+            hpo_path: path to HPO obo formatted file
             
-            if node_id not in ref_mapper:
-                ref_mapper[node_id] = []
+        Returns:
+            parser.header: obo header for file
+            hpo_entries: list of entries in HPO database
+        """
+        
+        parser = obo.Parser(hpo_path)
+        hpo_entries = []
+        for entry in parser:
+            hpo_entries.append(entry)
+        
+        return parser.headers, hpo_entries
+
+    def add_hpo_attributes_to_node(self, node_id, obo_tags):
+        """ add hpo attributes to a graph node
+        
+        Args:
+            graph: networkx graph object
+            node_id: string ID for a graph node
+            obo_tags: tags for an obo entry
             
-            ref_mapper[node_id].append(alt_id)
+        Returns:
+            nothing, updates the graph node within this function
+        """
+        
+        hpo_keys = ['comment', 'subset', 'xref', 'synonym', 'name', \
+                    'created_by', 'creation_date', 'def', 'alt_id']
+        
+        for key in hpo_keys:
+            if key in obo_tags:
+                self.g.node[node_id] = obo_tags[key]
 
-def check_if_obsolete(obo_tags):
-    """ checks if an "is_obsolete" flag is in the tags for an obo entry
-    
-    Args:
-        obo_tags: tags for an obo entry
-    
-    Returns:
-        True/False for whether the entry is obsolete
-    """
-    
-    if "is_obsolete" in obo_tags:
-        if str(obo_tags["is_obsolete"][0]) == "true":
-            return True 
-    
-    return False
-
-def create_hpo_graph(hpo_header, hpo_list):
-    """builds a networkx graph from obo parsed data
-    
-    Args:
-        hpo_header: header for obo file
-        hpo_list: list of obo entries
-    
-    Returns:
-        g: netwrokx graph object for hpo file
-        alt_id_mapper: dict to map from alternate HPO IDs to canonical HPO IDs
-        ref_id_mapper: dict to map from canonical HPO IDs to alternate HPO IDs
-    """
-    
-    g = nx.DiGraph()
-    
-    # add the hpo header values as attributes for the graph
-    for header_id in hpo_header:
-        g.graph[header_id] = hpo_header[header_id]
-    
-    # track alternate HPO IDs (since we use HPO IDs as node IDs)
-    alt_id_mapper = {}
-    ref_id_mapper = {}
-    
-    for entry in hpo_list:
-        # ignore obsolete HPO entries
-        if check_if_obsolete(entry.tags):
-            continue
+    def track_alt_ids(self, node_id, obo_tags):
+        """ track alternate HPO IDs, to map between alternate and canonical
         
-        node_id = str(entry.tags["id"][0])
-        g.add_node(node_id)
+        Args:
+            node_id: string ID for a graph node
+            obo_tags: tags for an obo entry
         
-        # make sure we can convert between HPO ID and their alternate IDs
-        track_alternate_ids(entry.tags, node_id, alt_id_mapper, ref_id_mapper)
+        Returns:
+            nothing, updates the dictionaries within this function
+        """
         
-        # include the attribute data for the node
-        add_hpo_attributes_to_node(g, node_id, entry.tags)
-        
-        # add the predecessors to the node
-        if "is_a" in entry.tags:
-            for predecessor in entry.tags["is_a"]:
-                predecessor = str(predecessor)
+        # make sure we can convert between the alternate IDs and their HPO ID
+        if "alt_id" in obo_tags:
+            for alt_id in obo_tags["alt_id"]:
+                alt_id  = str(alt_id)
+                self.alt_mapper[alt_id] = node_id
                 
-                # add the predecessor node to the graph (don't worry about attributes, we'll add 
-                # those when we parse the entry for the predecessor)
-                if node_id not in g:
-                    g.add_node(predecessor)
+                if node_id not in ref_mapper:
+                    self.ref_mapper[node_id] = []
                 
-                g.add_edge(predecessor, node_id)    
-   
-    return g, alt_id_mapper, ref_id_mapper
+                self.ref_mapper[node_id].append(alt_id)
 
+    def check_if_obsolete(obo_tags):
+        """ checks if an "is_obsolete" flag is in the tags for an obo entry
+        
+        Args:
+            obo_tags: tags for an obo entry
+        
+        Returns:
+            True/False for whether the entry is obsolete
+        """
+        
+        if "is_obsolete" in obo_tags:
+            if str(obo_tags["is_obsolete"][0]) == "true":
+                return True 
+        
+        return False
+
+    def create_hpo_graph(self, hpo_header, hpo_list):
+        """builds a networkx graph from obo parsed data
+        
+        Args:
+            hpo_header: header for obo file
+            hpo_list: list of obo entries
+        
+        Returns:
+            nothing
+        """
+        
+        self.g = nx.DiGraph()
+        
+        # add the hpo header values as attributes for the graph
+        for header_id in hpo_header:
+            g.graph[header_id] = hpo_header[header_id]
+        
+        # track alternate HPO IDs (since we use HPO IDs as node IDs)
+        self.alt_id_mapper = {}
+        self.ref_id_mapper = {}
+        
+        for entry in hpo_list:
+            tags = entry.tags
+            # ignore obsolete HPO entries
+            if self.check_if_obsolete(tags):
+                continue
+            
+            node_id = str(tags["id"][0])
+            self.g.add_node(node_id)
+            
+            # make sure we can convert between HPO ID and their alternate IDs
+            self.track_alt_ids(tags, node_id)
+            
+            # include the attribute data for the node
+            self.add_hpo_attributes_to_node(node_id, tags)
+            
+            # add the predecessors to the node
+            if "is_a" in tags:
+                for predecessor in tags["is_a"]:
+                    predecessor = str(predecessor)
+                    self.g.add_edge(predecessor, node_id)
+    
+    def get_graph(self):
+        """ passes the graph object for external functions
+        """
+        
+        try:
+            return self.g
+        except AttributeError:
+            return None
+            
+
+    
 
 def main():
-    hpo_filename = "/Volumes/jm33/code/phenotype_ontology_filter/hpo_data/hp.obo"
-    hpo_header, hpo = load_hpo_database(hpo_filename)
-    g = create_hpo_graph(hpo_header, hpo)
+    # build a graph of DDG2P terms, so we can trace paths between terms
+    hpo = loadHPONetwork(HPO_PATH)
+    graph = hpo.get_graph()
+    
+    ddg2p_genes = load_files.load_ddg2p(DDG2P_PATH)
+    proband_hpo_terms = load_files.load_participants_hpo_terms(PHENOTYPES_PATH, ALTERNATE_IDS_PATH)
+    genes_index, probands_index = load_candidate_genes(CANDIDATE_VARIANTS_PATH)
+    
+    # nx.draw(g)
+    # plt.savefig("test.png")
 
 if __name__ == '__main__':
     main()
