6ef2c77773a7fdc219de9c4fa0a56b9accf269ee
Jeremy McRae <jm33@sanger.ac.uk>
2015-07-23 14:01:03 +0100

    added license information to code files

--- hpo_similarity.py
+++ hpo_similarity.py
@@ -1,4 +1,25 @@
-""" assess similarity of HPO terms in sets of probands.
+"""
+Copyright (c) 2015 Wellcome Trust Sanger Institute
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+===================================================
+Assess similarity of HPO terms in sets of probands.
 
 If we have a set of probands who we know share some genetic feature in a gene,
 we would like to know what is the probability of them sharing sharing their
